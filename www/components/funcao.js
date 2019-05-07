c5b22cb2003acb1ac771e8d9adabe9f649faf641
luizamaro11 <luiz.amaro90@gmail.com>
2019-05-08 04:01:46 +0900

    js

--- www/components/funcao.js
+++ www/components/funcao.js
@@ -1,2 +1,17 @@
 // This is a JavaScript file
 
+function camera1(){
+      navigator.camera.getPicture(onSuccess, onFail, {
+      quality: 70,
+      destinationType: Camera.DestinationType.DATA_URL});
+      saveToPhotoAlbum: true;
+      
+      function onSuccess(ImageData) {
+          var image = document.getElementById('imagem');
+          image.src = "data:image/jpeg;base64," + ImageData;
+      }
+      function onFail(message) {
+          alert('Failed because: ' + message);
+      }
+}
+
